e445efc9276d061acdd2973313e623fa416d20c9
Julia Chen <juliachenjing@gmail.com>
2017-10-22 18:31:00 -0500

    test updated

---
+++ test.js
@@ -0,0 +1,19 @@
+As a creative, motivated and highly adaptable software engineer with 4 years of programming experience, combining excellent educational credentials with hands-on network server management and programming skills, I am certain that I possess the expertise and qualifications needed as as a Software Engineer.
+I bring proven strengths in analysis, team collaboration and project management, combined with deep proficiency in all aspects of the software development life cycle, and I am confident that my knowledge and experience will enable me to immediately contribute to the performance of your team.
+ 
+I am well versed in the use of Java, C++, Spring, Hibernate, HTML5, CSS3, RESTful, JSP, Servlet, Visual C++, PowerShell, Cloud Computing, SCOM, CDN, Active Directory, DNS, MySQL, MFC, Visual Studio, Eclipse, Windows Server and numerous additional cutting edge technologies.
+ 
+
+Highlights of my abilities:
+ 
+Rapidly adapting to and implementing emerging technologies and platforms to drive application versatility and performance.
+ 
+Coordinating multiple simultaneous projects and adeptly managing priorities while flourishing in demanding and fast paced working environments.
+ 
+Discovering and resolving an array of software issues through expert analysis, testing, troubleshooting and debugging skills.
+ 
+ 
+Please refer to the included resume for a more detailed overview of my talents and background, and feel free to contact me at your earliest convenience to schedule an interview. Thank you for your time and consideration, and I look forward to hearing from you soon.
+
+Thanks,
+Julia
